d70e1980f0661b8757a0fe8032f15bf079baf41b
flysha <flysha@live.com>
2017-01-15 16:01:56 +0800

    switch to desktop folder instead of full path.

--- LotteryGen/AppConfig.cs
+++ LotteryGen/AppConfig.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Environment;
 
 namespace LotteryGen
 {
     public static class AppConfig
     {
-        public static string Source => ConfigurationManager.AppSettings[nameof(Source)];
+        public static string Source => ConfigurationManager.AppSettings[nameof(Source)]
+            .Replace("%Desktop%", GetFolderPath(SpecialFolder.Desktop));
 
-        public static string Dest => ConfigurationManager.AppSettings[nameof(Dest)];
+        public static string Dest => ConfigurationManager.AppSettings[nameof(Dest)]
+            .Replace("%Desktop%", GetFolderPath(SpecialFolder.Desktop));
 
-        public static string ExcelPath => ConfigurationManager.AppSettings[nameof(ExcelPath)];
+        public static string ExcelPath => ConfigurationManager.AppSettings[nameof(ExcelPath)]
+            .Replace("%Desktop%", GetFolderPath(SpecialFolder.Desktop));
     }
 }
