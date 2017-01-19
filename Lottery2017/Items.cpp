47c430b5ca184e030d5e4a22cadff6955c09fa3c
flysha.zhou <sdflysha@qq.com>
2017-01-19 17:14:02 +0800

    fix a typo

--- Lottery2017/Items.cpp
+++ Lottery2017/Items.cpp
@@ -17,7 +17,7 @@ const std::vector<Item>& GetItems()
 	{
 		Item(L"一等奖-iPhone7",						1,  IDR_ITEM1),
 		Item(L"二等奖-机械键盘x2/Kindlex3",			5,  IDR_ITEM2),
-		Item(L"三等奖x5-小米净化器x5/呼吸套装x5",		10, IDR_ITEM4),
+		Item(L"三等奖-小米净化器x5/呼吸套装x5",		10, IDR_ITEM4),
 		Item(L"阳光普照奖x30-小熊酸奶机",				30, IDR_ITEM6),
 		Item(L"阳光普照奖x30-弓箭全钢化保鲜盒套装",	30, IDR_ITEM7),
 		Item(L"阳光普照奖x30-立白洗衣液",				30, IDR_ITEM8),
